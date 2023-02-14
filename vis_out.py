28e4601781fc18cf50491b1f1459737866dd0b4e
quyen228 <69098210+quyen228@users.noreply.github.com>
2023-02-14 18:10:11 +0700

    Update vis_out.py

--- vis_out.py
+++ vis_out.py
@@ -125,7 +125,7 @@ def test_fitb(args):
         kwargs = {'K': args.k, 'subset': args.subset,
                 'resampled': args.resampled, 'expand_outfit':args.expand_outfit}
 
-        for idx, question_adj, out_ids, choices_ids, labels, valid in enumerate(dl.yield_test_questions_K_edges(**kwargs)):
+        for idx, (question_adj, out_ids, choices_ids, labels, valid) in enumerate(dl.yield_test_questions_K_edges(**kwargs)):
             q_support = get_degree_supports(question_adj, config['degree'], adj_self_con=ADJ_SELF_CONNECTIONS, verbose=False)
             for i in range(1, len(q_support)):
                 q_support[i] = norm_adj(q_support[i])
