c00fb39d62fb4a5a9d86a752275eef57900f68b0
quyen228 <69098210+quyen228@users.noreply.github.com>
2023-02-16 02:03:13 +0700

    Update Polyvore.py

--- dataloaders/Polyvore.py
+++ dataloaders/Polyvore.py
@@ -198,9 +198,7 @@ class DataLoaderPolyvore(Dataloader):
                 if idx2clus[str(index)] == "null":
                     continue
                 for index_answer in full_choices: # indexes of possible choices answers    
-                    if index_answer in outfit_subset \
-                        or idx2clus[str(index_answer)] == "null" \ 
-                            or idx2clus[str(index_answer)] == idx2clus[str(index)]:
+                    if (index_answer in outfit_subset) or (idx2clus[str(index_answer)] == "null") or (idx2clus[str(index_answer)] == idx2clus[str(index)]):
                         continue
                     outfit_ids.append(index)
                     choices_ids.append(index_answer)
